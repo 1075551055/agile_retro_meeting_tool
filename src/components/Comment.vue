a116dc9d3e50a050a18ed62b233feabcba99f8bb
世民后人 <1075551055@qq.com>
2019-02-06 23:57:16 +0800

    dynamic ref
    dynamic ref

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -31,7 +31,7 @@
               <a href class="iconfont icon-jiahao"></a>
               <div contenteditable="true" draggable="true" id="mytest"></div>
             </div>
-          </div> -->
+          </div>-->
         </b-col>
         <b-col class="summary-comment-col">
           <!-- <div class="bottom">
@@ -39,7 +39,7 @@
               <a href class="iconfont icon-jiahao"></a>
               <div contenteditable="true"></div>
             </div>
-          </div> -->
+          </div>-->
         </b-col>
         <b-col class="summary-comment-col">
           <!-- <div class="bottom">
@@ -47,30 +47,46 @@
               <a href class="iconfont icon-jiahao"></a>
               <div contenteditable="true"></div>
             </div>
-          </div> -->
+          </div>-->
         </b-col>
       </b-row>
       <b-row>
         <b-col class="comment-col">
-            <div class="fake-textarea" draggable="true">
-              <a href class="iconfont icon-jiahao"></a>
-              <!-- <div contenteditable="true" draggable="true" @input="changeData($event)"></div> -->
-              <div contenteditable="true" draggable="true" :id="$uuid.v1()"></div>
-            </div>
+          <div class="fake-textarea" draggable="true">
+            <a href class="iconfont icon-jiahao"></a>
+            <!-- <div contenteditable="true" draggable="true" @input="changeData($event)"></div> -->
+            <div
+              contenteditable="true"
+              draggable="true"
+              :id="wellCommentTextarea[0].id"
+              :ref="wellCommentTextarea[0].ref"
+              data-commenttype="well"
+            ></div>
+          </div>
         </b-col>
         <b-col class="comment-col">
-            <div class="fake-textarea" draggable="true">
-              <a href class="iconfont icon-jiahao"></a>
-              <!-- <div contenteditable="true" draggable="true" @input="changeData($event)"></div> -->
-              <div contenteditable="true" draggable="true" :id="$uuid.v1()"></div>
-            </div>
+          <div class="fake-textarea" draggable="true">
+            <a href class="iconfont icon-jiahao"></a>
+            <!-- <div contenteditable="true" draggable="true" @input="changeData($event)"></div> -->
+            <div
+              contenteditable="true"
+              draggable="true"
+              :id="$uuid.v1()"
+              data-commenttype="notwell"
+            ></div>
+          </div>
         </b-col>
         <b-col class="comment-col">
-            <div class="fake-textarea" draggable="true">
-              <a href class="iconfont icon-jiahao"></a>
-              <!-- <div contenteditable="true" draggable="true" @input="changeData($event)"></div> -->
-              <div contenteditable="true" draggable="true" :id="$uuid.v1()"></div>
-            </div>
+          <div class="fake-textarea" draggable="true">
+            <a href class="iconfont icon-jiahao"></a>
+            <!-- <div contenteditable="true" draggable="true" @input="changeData($event)"></div> -->
+            <div
+              contenteditable="true"
+              draggable="true"
+              :id="$uuid.v1()"
+              data-commenttype="suggestion"
+            ></div>
+          </div>
         </b-col>
       </b-row>
     </b-container>
@@ -78,64 +94,101 @@
 </template>
 
 <script>
-import { uuid } from 'vue-uuid';
+import { uuid } from "vue-uuid";
 export default {
   data: function() {
     return {
-      msg: ""
+      wellCommentTextarea: [
+        {
+          id: uuid.v1(),
+          ref: uuid.v1()
+        }
+      ],
+      notwellCommentTextarea: [
+        {
+          id: uuid.v1(),
+          ref: uuid.v1()
+        }
+      ],
+      suggestionCommentTextarea: [
+        {
+          id: uuid.v1(),
+          ref: uuid.v1()
+        }
+      ],
+      commentTypes: {
+        well: "well",
+        notwell: "notwell",
+        suggestion: "suggestion"
+      }
     };
   },
-  methods:{
-    changeData: function(event){
+  methods: {
+    changeData: function(event) {
       // this.msg = event.target.innerText;
+    },
+    setDragDrop: function() {
+      // this.$refs.
+      let fakeTextAreas = document.querySelectorAll(".fake-textarea > div");
+      [].forEach.call(fakeTextAreas, el => {
+        let self = this;
+        el.ondragstart = function(e) {
+          // drag start
+          //console.log("dragstart");
+          e.dataTransfer.setData(
+            "text/html",
+            JSON.stringify({
+              id: e.target.id,
+              commentType: e.target.dataset.commenttype
+            })
+          );
+          console.log(e.target.dataset.commenttype);
+          // console.log(self.$refs[self.wellCommentTextarea[0].ref]);
+        };
+        el.ondragend = function(e) {
+          // after mouseup will trigger dragend
+          // console.log("dragend");
+        };
+        el.ondrag = function(e) {
+          // continue to trigger during drag
+        };
+        el.ondragleave = function(e) {
+          // will trigger dragleave when cursor leave the element
+          // console.log("dragleave");
+        };
+      });
+
+      let commentCols = document.querySelectorAll(".summary-comment-col");
+      Array.prototype.forEach.call(commentCols, el => {
+        let self = this;
+        el.ondragenter = function(e) {};
+        el.ondragover = function(e) {
+          // prevent browser default behaviour or it cannot trigger ondrop event
+          e.preventDefault();
+        };
+        el.ondrop = function(e) {
+          let idAndCommentTypeJson = JSON.parse(
+            e.dataTransfer.getData("text/html")
+          );
+          let id = idAndCommentTypeJson.id;
+          let commentType = idAndCommentTypeJson.commentType;
+          if (commentType === self.commentTypes.well) {
+          }
+          // self.$refs[self.wellCommentTextarea[0].ref]
+          let originalCommentEle = document.getElementById(id);
+          let parentElement = originalCommentEle.parentElement;
+          originalCommentEle.style = "width: 45%; height: 35%; display: inline-block; border:1px solid pink; overflow: auto; margin: 3px; background-color: #fff ; box-shadow: 3px 3px 6px #43a047";
+          e.target.appendChild(originalCommentEle);
+        };
+        
+        el.ondragleave = function(e) {
+          //console.log('ondragleave')
+        };
+      });
     }
   },
   mounted: function() {
-    let fakeTextAreas = document.querySelectorAll(".fake-textarea > div");
-    [].forEach.call(fakeTextAreas, function(el){
-      el.ondragstart = function(e){
-        // drag start
-        //console.log("dragstart");
-        e.dataTransfer.setData("text/html", e.target.id);
-        // console.log(e.target.id);
-      }
-      el.ondragend = function(e){
-        // after mouseup will trigger dragend
-        // console.log("dragend");
-      }
-      el.ondrag = function(e){
-        // continue to trigger during drag
-       
-      }
-      el.ondragleave = function(e){
-        // will trigger dragleave when cursor leave the element
-        // console.log("dragleave");
-      }
-    })
-
-    let commentCols = document.querySelectorAll(".summary-comment-col");
-    Array.prototype.forEach.call(commentCols, function(el) {
-      el.ondragenter = function(e) {
-        //console.log("ondragenter")
-      }
-      el.ondragover = function(e){
-        // console.log("ondragover")
-        e.preventDefault();
-        
-      }
-      el.ondrop = function(e){
-        //console.log('ondragdrop')
-        let id = e.dataTransfer.getData("text/html");
-        let originalCommentEle = document.getElementById(id);
-        let newCommentEle = originalCommentEle.cloneNode(true);
-        newCommentEle.id = uuid.v1();
-        newCommentEle.style = "width: 45%; height: 120px; display: inline-block; overflow: auto; margin: 8px; background-color: #fff ; box-shadow: 3px 3px 6px #43a047";
-        e.target.appendChild(newCommentEle);
-      }
-      el.ondragleave = function(e) {
-        //console.log('ondragleave')
-      }
-    })
+    this.setDragDrop();
   }
 };
 </script>
@@ -251,38 +304,38 @@ export default {
       border-right: 1px dashed #ccc;
       max-width: 33.3333333333333%;
       .fake-textarea {
-          border: 1px solid #ccc;
-          border-radius: 8px;
-          box-shadow: 5px 5px 8px rgba(0, 0, 0, 0.6);
-          margin-top: 15px;
-          margin-bottom: 25px;
-          position: relative;
-          max-width: 100%;
-          > div {
-            display: inline-block;
-            min-height: 60px;
-            max-height: 120px;
-            height: 100%;
-            width: calc(100% - 45px);
-            width: -webkit-calc(100% - 45px);
-            width: -moz-calc(100% - 45px);
-            max-width: calc(100% - 45px);
-            margin-left: 45px;
-            overflow: auto;
-            outline: none;
-            // border: 1px solid red;
-          }
-          > a {
-            display: inline;
-            position: absolute;
-            top: 50%;
-            transform: translateY(-50%);
-            padding-left: 8px;
-            font-size: 30px;
-            color: #aaa;
-            text-decoration: none;
-          }
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        box-shadow: 5px 5px 8px rgba(0, 0, 0, 0.6);
+        margin-top: 15px;
+        margin-bottom: 25px;
+        position: relative;
+        max-width: 100%;
+        > div {
+          display: inline-block;
+          min-height: 60px;
+          max-height: 120px;
+          height: 100%;
+          width: calc(100% - 45px);
+          width: -webkit-calc(100% - 45px);
+          width: -moz-calc(100% - 45px);
+          max-width: calc(100% - 45px);
+          margin-left: 45px;
+          overflow: auto;
+          outline: none;
+          // border: 1px solid red;
         }
+        > a {
+          display: inline;
+          position: absolute;
+          top: 50%;
+          transform: translateY(-50%);
+          padding-left: 8px;
+          font-size: 30px;
+          color: #aaa;
+          text-decoration: none;
+        }
+      }
     }
   }
 }
